377a59ccd26220ced8bad8476d281d162d2c5fb7
Bo.Schwarzstein@gmail.com <Bo.Schwarzstein@gmail.com@31fcc30c-cce6-11de-a502-cffa054868e3>
2009-11-09 10:16:32 +0000

    Added support for CUDA.

---
+++ trunk/VPRUtil.cu
@@ -0,0 +1,81 @@
+﻿#include "VPRUtil.h"
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/copy.h>
+
+#include <boost/scoped_array.hpp>
+
+struct TentKernel
+{
+	TentKernel(const float3& TheSP)
+	{
+		SP.x = TheSP.x;
+		SP.y = TheSP.y;
+		SP.z = TheSP.z;
+	}
+	
+	__host__ __device__
+	float operator()(const float3 P, const float Sum)
+	{
+		return Sum + (1.0f-abs(P.x-SP.x))*(1.0f-abs(P.y-SP.y))*(1.0-abs(P.z-SP.z));
+	}
+	
+	float3 SP;
+};
+
+bool MakeTent(const size_t Len, const float3* Points, const int Res[3], const float3& Min, const float3& Max, float* Data)
+{
+	try
+	{
+		float3 Extent;
+		Extent.x = Max.x - Min.x;
+		Extent.y = Max.y - Min.y;
+		Extent.z = Max.z - Min.z;
+		
+		float3 Delta;
+		Delta.x = Extent.x / (float)Res[0];
+		Delta.y = Extent.y / (float)Res[1];
+		Delta.z = Extent.z / (float)Res[2];
+		
+		const int TotalCount = Res[0]*Res[1]*Res[2];
+		
+		boost::scoped_array<float3> GridPosHst(new float3[TotalCount]);
+		thrust::device_vector<float3> GridPosDev(TotalCount);
+		thrust::device_vector<float> TentFuncDev(TotalCount,0.0f);
+		
+		for( int z=0; z<Res[2]; ++z )
+		{
+			int ZOffset = z*Res[0]*Res[1];
+			for( int y=0; y<Res[1]; ++y )
+			{
+				int YOffset = y*Res[0];
+				for( int x=0; x<Res[0]; ++x )
+				{
+					int XOffset = x;
+					float3 P;
+					P.x = ( (float)x+0.5 )*Delta.x + Min.x;
+					P.y = ( (float)y+0.5 )*Delta.y + Min.y;
+					P.z = ( (float)z+0.5 )*Delta.z + Min.z;
+					GridPosHst[ZOffset+YOffset+XOffset] = P;
+				}
+			}
+		}
+		thrust::copy(GridPosHst.get(),GridPosHst.get()+TotalCount,GridPosDev.begin());
+		GridPosHst.reset();
+		
+		for( size_t i=0; i<Len; ++i )
+		{
+			thrust::transform(GridPosDev.begin(),GridPosDev.end(),TentFuncDev.begin(),TentFuncDev.end(),TentKernel(Points[i]));
+		}
+		
+		thrust::copy(TentFuncDev.begin(),TentFuncDev.end(),Data);
+		
+	}catch(...)
+	{
+		return false;
+	}
+	
+	
+	return true;
+}
---
+++ trunk/VPRUtil.h
@@ -0,0 +1,10 @@
+﻿#ifndef VPRUTIL_H
+
+#include <builtin_types.h>
+
+/** \brief Make a tent function in volume from points.
+ * The first must contain the object coordinate system points.
+ */
+bool MakeTent(const size_t, const float3*, const int[3], const float3&, const float3&, float*);
+
+#endif
